dfaa309530f4e7d646d2d83c9ccd4c39a29c557e
muhammedsheikhh <mosheikh78690@gmail.com>
2022-02-15 20:27:50 -0600

    selectors created

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  props: ["note"],
+};
+</script>
+<template>
+  <div class="note-selector">
+    <p class="note-selector-title">{{ note.body }}</p>
+    <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,24 @@
+<script>
+import NoteSelector from "./NoteSelector.vue";
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() - 2000000 },
+        { id: 2, body: "This is a second test", timestamp: Date.now() - 1000000 },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
+  </div>
+</template>
